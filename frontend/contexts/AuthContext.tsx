0211a2956cdec8e2d1f77b5ec0d01592d35d933f
maucaruso <carusojr@outlook.com>
2022-08-09 08:09:33 -0300

    axios refresh token

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useState } from "react";
-import { setCookie } from "nookies";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import Router from "next/router";
 
 import { api } from "../services/api";
@@ -27,9 +27,30 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token');
+  destroyCookie(undefined, 'nextauth.refreshToken');
+  
+  Router.push('/');
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user;
+  
+  useEffect(() => {
+    const { 'nextauth.token': token } = parseCookies();
+    
+    if (token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data;
+        
+        setUser({ email, permissions, roles });
+      }).catch(() => {
+        signOut();
+      });
+    }
+  }, []);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -55,6 +76,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
         permissions,
         roles,
       });
+      
+      api.defaults.headers['Authorization'] = `Bearer ${token}`;
 
       Router.push("/dashboard");
     } catch (err) {

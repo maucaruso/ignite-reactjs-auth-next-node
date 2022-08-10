05045dcdd983f6ee34a858de69dd61857a5e9a8d
maucaruso <carusojr@outlook.com>
2022-08-10 08:09:16 -0300

    ssr auth and refresh token

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { api } from "../services/api";
+import { setupApiClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
@@ -16,3 +18,16 @@ export default function Dashboard() {
     
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupApiClient(ctx);
+  
+  const response = await apiClient.get('/me');
+  
+  console.log(response.data);
+  
+  return {
+    props: {
+    }
+  }
+})
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,6 +4,7 @@ import { parseCookies } from 'nookies';
 
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 export default function Home() {
   const [email, setEmail] = useState('');
@@ -31,19 +32,9 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-  
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {
     }
   }
-  
-  return {
-    props: {}
-  }
-}
+})

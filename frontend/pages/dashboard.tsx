93b972efc7c458d0757385ec5930fb4f46a93894
maucaruso <carusojr@outlook.com>
2022-08-12 08:41:44 -0300

    permissions control

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
@@ -6,28 +7,33 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-  
+
   useEffect(() => {
-    api.get('/me')
-      .then(response => console.log(response))
-      .catch(err => console.log(err));
+    api
+      .get("/me")
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   }, []);
-  
+
   return (
-    <h1>Dashboard: {user?.email}</h1>
-    
-  )
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupApiClient(ctx);
-  
-  const response = await apiClient.get('/me');
-  
+
+  const response = await apiClient.get("/me");
+
   console.log(response.data);
-  
+
   return {
-    props: {
-    }
-  }
-})
+    props: {},
+  };
+});
